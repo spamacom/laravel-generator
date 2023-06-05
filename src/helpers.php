33bca1163ce1f1f68a3a96a8896a4eb030a050b3
Spama Store <61113502+spamacom@users.noreply.github.com>
2023-06-05 15:00:10 +0300

    Update helpers.php

--- src/helpers.php
+++ src/helpers.php
@@ -74,3 +74,48 @@ if (!function_exists('create_resource_route_names')) {
         return $result;
     }
 }
+
+if (!function_exists('get_template_file_path')) {
+    /**
+     * get path for template file.
+     *
+     * @param string $templateName
+     * @param string $templateType
+     *
+     * @return string
+     */
+    function get_template_file_path($templateName, $templateType)
+    {
+        $templateName = str_replace('.', '/', $templateName);
+
+        $templatesPath = config(
+            'infyom.laravel_generator.path.templates_dir',
+            base_path('resources/infyom/infyom-generator-templates/')
+        );
+
+        $path = $templatesPath . $templateName . '.stub';
+
+        if (file_exists($path)) {
+            return $path;
+        }
+
+        return base_path('vendor/infyomlabs/' . $templateType . '/templates/' . $templateName . '.stub');
+    }
+}
+
+if (!function_exists('get_template')) {
+    /**
+     * get template contents.
+     *
+     * @param string $templateName
+     * @param string $templateType
+     *
+     * @return string
+     */
+    function get_template($templateName, $templateType)
+    {
+        $path = get_template_file_path($templateName, $templateType);
+
+        return file_get_contents($path);
+    }
+}
